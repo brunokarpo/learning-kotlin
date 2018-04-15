5f9207d839e1da57b8a2a192b2342f6af1407fe5
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 14:59:44 -0300

    Learning about Float type

---
+++ src/VariableTypeFloat.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+
+    var myFloat = 0.5
+    println(myFloat)
+
+    var newFloat: Float? = null
+    println(newFloat)
+
+    /*
+     * We need to put this 'F' on the final of number to compiler understand that we are trying to put a Float inside the variable
+     */
+    newFloat = 23.4F
+    println(newFloat)
+}
