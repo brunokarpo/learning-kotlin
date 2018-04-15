39391bcb43bfd901bfe0592ebf5d70b633f4e302
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 17:35:59 -0300

    Learning about if/else statements in Kotlin

---
+++ src/IfElseStatements.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+    var firstNumber = 10
+    var secondNumber = 20
+
+    if( firstNumber > secondNumber) {
+        println("Yes")
+    } else {
+        println("No")
+    }
+
+    if(firstNumber < secondNumber)
+        println("Yes")
+    else
+        println("No")
+
+    if (firstNumber < secondNumber) println("Yes")   else println("No")
+}
