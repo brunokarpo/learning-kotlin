3a82f3935dac3edb04bfef05987663de299b2006
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 12:04:33 -0300

    Creating another Hello World, but printing a lot of lines on console

---
+++ src/HelloKotlin.kt
@@ -0,0 +1,6 @@
+fun main( args: Array<String>) {
+
+    println("Hello Kotlin")
+    println("I love Kotlin")
+    println(" I really wanted print a lot of lines, but, for me it's boring... sorry! ")
+}
