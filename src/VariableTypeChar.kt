45afc0eb1c167e031e4c91d76238dc9797333489
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 15:00:22 -0300

    Learning about Char type

---
+++ src/VariableTypeChar.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    var myChar = 'a'
+    println(myChar)
+
+    var newChar: Char? = null
+    println(newChar)
+
+    newChar = 'M'
+    println(newChar)
+
+    println("$myChar $newChar")
+}
