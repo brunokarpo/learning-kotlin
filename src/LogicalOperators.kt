1ca0ddc5bf6afd7cce16a4ea02dbd80632a53290
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 17:41:37 -0300

    Learning about Logical operators in Kotlin, but that isn't new for me

---
+++ src/LogicalOperators.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    var firstNumber = 5
+    var secondNumber = 6
+    var thirdNumber = 10
+
+    if((firstNumber > secondNumber)
+            && (thirdNumber < firstNumber))
+        println("Yes")
+    else
+        println("Nope")
+
+
+    /*
+     * There isn't so much to learn here... go away. I've six years of experience in that...
+     */
+}
