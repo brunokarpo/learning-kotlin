14496441873cc4f1dee83a256a4ab84019fec8a5
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 17:24:30 -0300

    Learning about conditional Operators in Kotlin

---
+++ src/CondicionalOperators.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    val firstNumber = 5
+    val secondNumber = 6
+
+    println(firstNumber > secondNumber) // false
+    println(firstNumber < secondNumber) // true
+    println(firstNumber > firstNumber) // false
+    println(firstNumber >= firstNumber) // true
+    println(secondNumber < secondNumber) // false
+    println(secondNumber <= secondNumber) // true
+    println(firstNumber == secondNumber) // false
+    println(firstNumber != secondNumber) // true
+
+}
