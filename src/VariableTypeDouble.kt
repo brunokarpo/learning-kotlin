a5ca15252b907c0daa03133310dd4786d49aed25
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 12:53:45 -0300

    Learning about Double types

---
+++ src/VariableTypeDouble.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+
+    var myDouble= 23.01
+    var newDouble: Double = 44.6
+
+    println("Here is my number $myDouble and my new double is $newDouble and when I sum these i got ${myDouble + newDouble}")
+
+}
