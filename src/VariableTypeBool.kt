0aee9b044073ce72bcf337a1b52bb3c6a5c7d987
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 12:58:42 -0300

    Learning about Boolean values

---
+++ src/VariableTypeBool.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var myBool = true
+    println(myBool)
+
+    var newBool: Boolean? = null
+    println(newBool)
+
+    newBool = false
+    println(newBool)
+}
