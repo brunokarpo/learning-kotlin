2fc3b5b375085bbea14701c44baca18e2ed3d78f
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 12:09:26 -0300

    Learning how to comment in my code.

--- src/HelloKotlin.kt
+++ src/HelloKotlin.kt
@@ -1,6 +1,14 @@
+/**
+ * This is a Documentation of my function
+ */
 fun main( args: Array<String>) {
 
+    // This is a line of comment
     println("Hello Kotlin")
     println("I love Kotlin")
     println(" I really wanted print a lot of lines, but, for me it's boring... sorry! ")
+
+    /*
+     * This is a block of comments
+     */
 }
