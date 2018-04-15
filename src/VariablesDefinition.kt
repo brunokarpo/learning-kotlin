66c6db3c6f5afec153bfa4dc05063a27097e4bfd
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-15 12:17:06 -0300

    Learning how to define a variable and printing on console

---
+++ src/VariablesDefinition.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var name = "Bruno"
+    var lastname = "N. de Oliveira"
+    var fishname = "Francisco"
+
+    println(name + " " + lastname + " has a fish named: " + fishname);
+}
