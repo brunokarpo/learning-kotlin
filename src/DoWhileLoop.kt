9a70c253c15a2494dd643c35933510f364e22223
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-16 11:56:15 -0300

    Learning about do while loops on Kotlin

---
+++ src/DoWhileLoop.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var counter = 0
+
+    do {
+
+        if (counter % 2 == 0)  println("$counter is even") else  println("$counter is odd")
+
+    } while( ++counter < 30)
+
+}
