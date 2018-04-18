b518b94a41cb4f54afd5558259505a8f283ea1e5
Bruno Nogueira de Oliveira <ti.brunonogueira@gmail.com>
2018-04-18 20:20:30 -0300

    Learning outher way to iterate by array elements

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -13,6 +13,10 @@ fun main(args: Array<String>) {
     mArray2[4] = 33333
     mArray2.forEach { element -> println("Item: $element") }
 
+    for(index in 0..mArray2.size -1) {
+        println("Item: ${mArray2[index]}")
+    }
+
     var mArray3 = Array<String>(5){"Mandona"}
     mArray3.iterator().forEach { element -> println("Item '$element'") }
 
